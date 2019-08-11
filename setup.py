dbd9423d82e605e66ba0a077ad478fc7933023b6
Eduardo Enriqez (eduzen) <eduardo.a.enriquez@gmail.com>
2019-08-11 19:39:16 +0200

    Fixin dependencies

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-djangorestframework',
-    version='0.1.0',
+    version='0.1.1',
     author='Eduardo Enriquez',
     author_email='eduardo.a.enriquez@gmail.com',
     maintainer='Eduardo Enriquez',
